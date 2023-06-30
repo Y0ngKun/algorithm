17539c416ee9fccbf1b87d2aca7bd2c96308ea71
Yong <cksm00@naver.com>
2023-07-01 00:23:47 +0900

    Time: 0 ms (100%), Space: 43.1 MB (26.55%) - LeetHub

---
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int sum = 0;
+        int comparison = 0;
+
+        for(int i = 0; i < accounts.length ; i++){
+
+            for(int j = 0; j < accounts[i].length ; j++){
+                sum += accounts[i][j];
+            }
+            if(sum > comparison){
+                comparison = sum;
+            }
+            sum = sum - sum;
+        }
+
+        return comparison;
+        
+       
+    }
+}
