d3feece73bef58b1f80288de65dc7c31c255815c
Yong <cksm00@naver.com>
2023-10-04 10:17:17 +0900

    [level 0] Title: 모스부호 (1), Time: 3.48 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        Map<String,String> morse = new HashMap<String, String>();
+        morse.put(".-", "a");
+        morse.put("-...", "b");
+        morse.put("-.-.", "c");
+        morse.put("-..", "d");
+        morse.put(".", "e");
+        morse.put("..-.", "f");
+        morse.put("--.", "g");
+        morse.put("....", "h");
+        morse.put("..", "i");
+        morse.put(".---", "j");
+        morse.put("-.-", "k");
+        morse.put(".-..", "l");
+        morse.put("--", "m");
+        morse.put("-.", "n");
+        morse.put("---", "o");
+        morse.put(".--.", "p");
+        morse.put("--.-", "q");
+        morse.put(".-.", "r");
+        morse.put("...", "s");
+        morse.put("-", "t");
+        morse.put("..-", "u");
+        morse.put("...-", "v");
+        morse.put(".--", "w");
+        morse.put("-..-", "x");
+        morse.put("-.--", "y");
+        morse.put("--..", "z");
+        
+        for(int i = 0; i < letter.split(" ").length; i++ ) {
+        	answer += morse.get(letter.split(" ")[i]);
+        }
+        return answer;
+    }
+}
