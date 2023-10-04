e8599a93247eecc9de0c5d110286dd8cd14c03ec
Yong <cksm00@naver.com>
2023-10-05 00:13:51 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 7.87 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/README.md
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숨어있는 숫자의 덧셈 (1) - 120851 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120851) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120851?language=java) 
 
 ### 성능 요약
 
-메모리: 76.1 MB, 시간: 7.07 ms
+메모리: 76.9 MB, 시간: 7.87 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        my_string = my_string.replaceAll("[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z,"
-        		+ "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]", "");
+        my_string = my_string.replaceAll("[^0-9]", "");
         for(int i = 0; i <my_string.length(); i++) {
         	answer += Integer.parseInt(my_string.charAt(i)+"");
         }
