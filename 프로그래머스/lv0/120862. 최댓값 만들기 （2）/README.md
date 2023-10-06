a86475da2bdceff851184d433f45999836194041
Yong <cksm00@naver.com>
2023-10-06 13:02:38 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.02 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/README.md
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (2) - 120862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862?language=java) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 0.20 ms
+메모리: 70.3 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -4,11 +4,10 @@ class Solution {
         answer = numbers[0] * numbers[1];
         for(int i =0; i < numbers.length; i++) {
         	for(int j=1; j<numbers.length; j++) {
-        		if(i!=j && (numbers[i]*numbers[j] >= answer) )
+        		if(j>i && i!=j && numbers[i]*numbers[j] > answer)
         			answer = numbers[i]*numbers[j];
         	}
         }
-        System.out.println(answer);
         return answer;
     }
 }
