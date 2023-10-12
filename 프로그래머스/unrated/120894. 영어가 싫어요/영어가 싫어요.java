2f084521165cf4fb4785aee6d5bf7520143188a7
Yong <cksm00@naver.com>
2023-10-13 00:18:29 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.57 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(String numbers) {
+            numbers = numbers.replaceAll("zero", "0");
+	        numbers = numbers.replaceAll("one", "1");
+	        numbers = numbers.replaceAll("two", "2");
+	        numbers = numbers.replaceAll("three", "3");
+	        numbers = numbers.replaceAll("four", "4");
+	        numbers = numbers.replaceAll("five", "5");
+	        numbers = numbers.replaceAll("six", "6");
+	        numbers = numbers.replaceAll("seven", "7");
+	        numbers = numbers.replaceAll("eight", "8");
+	        numbers = numbers.replaceAll("nine", "9");
+	        return Long.valueOf(numbers);
+	    }
+}
