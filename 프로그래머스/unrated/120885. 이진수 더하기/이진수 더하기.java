eee078b21a37ac9fee033cab256ff5cb4f99e5b3
Yong <cksm00@naver.com>
2023-10-12 13:03:18 +0900

    [level unrated] Title: 이진수 더하기, Time: 0.03 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/unrated/120885. 이진수 더하기/이진수 더하기.java
@@ -1,6 +1,5 @@
 class Solution {
    public String solution(String bin1, String bin2) {
-	        int answer = Integer.parseUnsignedInt(bin1, 2) + Integer.parseUnsignedInt(bin2, 2);
-	        return Integer.toString(answer, 2);
+	        return Integer.toString(Integer.parseUnsignedInt(bin1, 2) + Integer.parseUnsignedInt(bin2, 2), 2);
 	    }
 }
