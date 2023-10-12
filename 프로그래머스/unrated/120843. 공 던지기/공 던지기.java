9bbd0083e160068bf5f8be2e11c8db5500d47af8
Yong <cksm00@naver.com>
2023-10-12 22:36:40 +0900

    [level unrated] Title: 공 던지기, Time: 0.07 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/unrated/120843. 공 던지기/공 던지기.java
@@ -1,30 +1,27 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-		int answer = 0;
+		int answer = 0, i = 0, count =0;
 
-		int i = 1, count =0 ,j = 0;
 		while(true) {
-			answer = numbers[j];
-			if(count >= k) break;
-			i = i+2;
-			++count;
-
-			if(i%2==0 && i>numbers.length) {
-				answer = numbers[numbers.length-2];
+			if(i==numbers.length-2){
+				answer = numbers[i];
+				count++;
+				i = 0;
+				if(count >= k) break;
+			}
+			if(i==numbers.length-1){
+				answer = numbers[i];
+				count++;
+				i = 1;
+				if(count >= k) break;
+			}
+			else{
+				answer = numbers[i];
 				++count;
 				if(count >= k) break;
-				j = 0;
+				i += 2;
 			}
-
-			if(i%3==0 && i>=numbers.length) {
-				answer = numbers[numbers.length-1];
-				++count;
-				if(count >= k) break;
-				j = 1;
-			}
-			
 		}
-		System.out.println(answer);
 		return answer;
 	}
 }
