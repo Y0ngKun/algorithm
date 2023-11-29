1d66002ff0b2ecfe0e826c92a9f7ab123cb545fc
Yong <cksm00@naver.com>
2023-11-29 19:29:39 +0900

    [Bronze V] Title: 사분면 고르기, Time: 212 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+				Scanner sc = new Scanner(System.in);
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				if(a>0) {
+					if(b>0){
+						System.out.println("1");
+					}
+					else {
+						System.out.println("4");
+					}
+				}
+				else if(a<0) {
+					if(b>0) System.out.println("2");
+					else {
+						System.out.println("3");
+					}
+				}
+	}
+}
